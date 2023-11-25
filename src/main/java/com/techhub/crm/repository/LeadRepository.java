6375918891a3b0f74eca3c2e9680abf19e72469d
shobhan28 <shobhan28m@gmail.com>
2023-11-25 14:34:40 +0530

    method declaration for a query method in a Spring Data JPA repository interface

---
+++ src/main/java/com/techhub/crm/repository/LeadRepository.java
@@ -0,0 +1,19 @@
+package com.techhub.crm.repository;
+
+import com.techhub.crm.entity.Lead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LeadRepository extends JpaRepository <Lead, String> {
+
+    // before saving the lead, i want to check whether that email exists
+    Optional<Lead> findByEmail(String email);
+
+    Optional<Lead> findByMobile(long mobile);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByMobile(String mobile);
+
+}
