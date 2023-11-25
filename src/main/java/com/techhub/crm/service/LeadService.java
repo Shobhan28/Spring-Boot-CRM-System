e545dfb0b0b940e6d038f6c0cbfc06fd0b817cf2
shobhan28 <shobhan28m@gmail.com>
2023-11-25 17:59:49 +0530

    created getAllLeads method

--- src/main/java/com/techhub/crm/service/LeadService.java
+++ src/main/java/com/techhub/crm/service/LeadService.java
@@ -4,10 +4,14 @@ package com.techhub.crm.service;
 import com.techhub.crm.payload.LeadDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface LeadService {
 
     LeadDto createLead(LeadDto leadDto);
 
     void deleteLeadById(String lid);
+
+    List<LeadDto> getAllLeads();
 }
