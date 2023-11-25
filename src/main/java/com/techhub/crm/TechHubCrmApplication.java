f5e98da350c1126ec08e4e6489cf922df903372d
shobhan28 <shobhan28m@gmail.com>
2023-11-25 12:31:06 +0530

    first commit

---
+++ src/main/java/com/techhub/crm/TechHubCrmApplication.java
@@ -0,0 +1,13 @@
+package com.techhub.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHubCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHubCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techhub/crm/TechHubCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techhub.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHubCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
