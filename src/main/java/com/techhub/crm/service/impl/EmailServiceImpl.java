8bf66371fe840faf44e1596b303f6755508264a7
shobhan28 <shobhan28m@gmail.com>
2023-11-25 20:26:16 +0530

    overridden sendEmail method

---
+++ src/main/java/com/techhub/crm/service/impl/EmailServiceImpl.java
@@ -0,0 +1,66 @@
+package com.techhub.crm.service.impl;
+
+import com.techhub.crm.entity.Email;
+import com.techhub.crm.entity.Lead;
+import com.techhub.crm.exception.LeadExist;
+import com.techhub.crm.payload.EmailDto;
+import com.techhub.crm.repository.EmailRepository;
+import com.techhub.crm.repository.LeadRepository;
+import com.techhub.crm.service.EmailService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private EmailRepository emailRepo;
+    private LeadRepository leadRepo;
+
+    public EmailServiceImpl(EmailRepository emailRepo, LeadRepository leadRepo) {
+        this.emailRepo = emailRepo;
+        this.leadRepo = leadRepo;
+    }
+
+    @Override
+    public EmailDto sendEmail(EmailDto emailDto) {
+        Lead lead = leadRepo.findByEmail(emailDto.getTo()).orElseThrow(
+                () -> new LeadExist("Email id not Registered- " + emailDto.getTo())
+        );
+
+        // code for sending email
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(emailDto.getFrom());
+        mailMessage.setTo(emailDto.getTo());
+        mailMessage.setSubject(emailDto.getSubject());
+        mailMessage.setText(emailDto.getMessage());
+
+        javaMailSender.send(mailMessage);
+
+        Email email = mapToEntity(emailDto);
+        String emailid = UUID.randomUUID().toString();
+        email.setEid(emailid);
+
+        Email sentEmail = emailRepo.save(email);
+        return mapToDto(sentEmail);
+
+    }
+
+    private Email mapToEntity(EmailDto emailDto) {
+        return modelMapper.map(emailDto, Email.class);
+    }
+
+    private EmailDto mapToDto(Email email) {
+        return modelMapper.map(email, EmailDto.class);
+    }
+}
