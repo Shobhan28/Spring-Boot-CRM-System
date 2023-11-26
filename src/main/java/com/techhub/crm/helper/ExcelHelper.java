54add03a172c15f4b27adb6ce9d71b6ef9e6252d
shobhan28 <shobhan28m@gmail.com>
2023-11-26 20:48:00 +0530

    Implement Excel Helper Class

---
+++ src/main/java/com/techhub/crm/helper/ExcelHelper.java
@@ -0,0 +1,55 @@
+package com.techhub.crm.helper;
+
+import com.techhub.crm.entity.Lead;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+public class ExcelHelper {
+
+    public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    static String[] HEADERs = {"LeadId", "First Name", "Last Name", "Email", "Mobile", "Address", "Company Name", "Description", "Lead Status"};
+    static String SHEET = "Leads";
+
+    public static ByteArrayInputStream leadsToExcel(List<Lead> leads) {
+
+        try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+            Sheet sheet = workbook.createSheet(SHEET);
+
+            // Header
+            Row headerRow = sheet.createRow(0);
+
+            for (int col = 0; col < HEADERs.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellValue(HEADERs[col]);
+            }
+
+            int rowIdx = 1;
+            for (Lead lead : leads) {
+                Row row = sheet.createRow(rowIdx++);
+
+                row.createCell(0).setCellValue(lead.getLeadId());
+                row.createCell(1).setCellValue(lead.getFirstName());
+                row.createCell(2).setCellValue(lead.getLastName());
+                row.createCell(3).setCellValue(lead.getEmail());
+                row.createCell(4).setCellValue(lead.getMobile());
+                row.createCell(6).setCellValue(lead.getAddress());
+                row.createCell(8).setCellValue(lead.getCompanyName());
+                row.createCell(7).setCellValue(lead.getDescription());
+                row.createCell(9).setCellValue(lead.getLeadStatus());
+            }
+
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
+        }
+    }
+}
