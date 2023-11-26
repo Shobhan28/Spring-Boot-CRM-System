1f0c01a34bc57e656006f43d78c696f382d206f2
shobhan28 <shobhan28m@gmail.com>
2023-11-26 14:57:48 +0530

    implemented createContact method

--- src/main/java/com/techhub/crm/service/ContactService.java
+++ src/main/java/com/techhub/crm/service/ContactService.java
@@ -4,5 +4,7 @@ import com.techhub.crm.payload.ContactDto;
 
 public interface ContactService {
 
-
+    ContactDto createContact(String leadId);
+    // Based on the leadId it will get the lead,
+    // copy that to contact and delete the lead.
 }
