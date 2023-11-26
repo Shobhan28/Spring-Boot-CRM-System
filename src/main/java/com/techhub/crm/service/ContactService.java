baf073c287ab79addb311590dc64c6365340f497
shobhan28 <shobhan28m@gmail.com>
2023-11-26 14:57:10 +0530

    created service layer for contact

--- src/main/java/com/techhub/crm/service/ContactService.java
+++ src/main/java/com/techhub/crm/service/ContactService.java
@@ -4,7 +4,5 @@ import com.techhub.crm.payload.ContactDto;
 
 public interface ContactService {
 
-    ContactDto createContact(String leadId);
-    // Based on the leadId it will get the lead,
-    // copy that to contact and delete the lead.
+
 }
