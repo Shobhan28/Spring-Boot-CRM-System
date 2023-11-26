9f43975e9d9846c69985dcccc2f2fa7dfe3c58d0
shobhan28 <shobhan28m@gmail.com>
2023-11-26 16:11:48 +0530

    Build Repository layer for Contact

--- src/main/java/com/techhub/crm/repository/ContactRepository.java
+++ src/main/java/com/techhub/crm/repository/ContactRepository.java
@@ -3,7 +3,9 @@ package com.techhub.crm.repository;
 import com.techhub.crm.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository extends JpaRepository<Contact,String> {
+import java.util.Optional;
 
+public interface ContactRepository extends JpaRepository<Contact, String> {
 
+    Optional<Contact> findByEmail(String email);
 }
