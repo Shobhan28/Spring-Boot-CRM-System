2a7469f793949d1f0d9661be223239546bb973b2
shobhan28 <shobhan28m@gmail.com>
2023-11-26 16:07:52 +0530

    Also need to change Lead fields

--- src/main/java/com/techhub/crm/payload/LeadDto.java
+++ src/main/java/com/techhub/crm/payload/LeadDto.java
@@ -9,15 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 public class LeadDto {
 
-private String lid;
+private String leadId;
 private String firstName;
 private String lastName;
 private String email;
 private long mobile;
-private String leadType;
 private String address;
-private String designation;
-private String company;
-private String note;
-
+private String companyName;
+private String description;
+private String leadStatus;
 }
